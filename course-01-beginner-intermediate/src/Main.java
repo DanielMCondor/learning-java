a4a75c7cef5432612b39c9cf42a0535b43f0d882
DanielMCondor <dmantillacondor@gmail.com>
2023-04-24 17:01:03 -0500

    my first hello world in java

--- course-01-beginner-intermediate/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Integer age = 25;
-        String name = "Daniel";
-        String phrase = "Hello, my name is " + name + " and I am " + String.valueOf(age) + " years old.";
-        System.out.println(phrase);
-    }
-}
