99980c9c918df321561bdcf39eaf916c71f54fa2
DanielMCondor <dmantillacondor@gmail.com>
2023-04-24 17:08:09 -0500

    text concatenation

---
+++ course-01-beginner-intermediate/src/Concatenation.java
@@ -0,0 +1,8 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        Integer age = 26;
+        String name = "Daniel";
+        String phrase = "Hello, my name is " + name + " and I am " + String.valueOf(age) + " years old.";
+        System.out.println(phrase);
+    }
+}
